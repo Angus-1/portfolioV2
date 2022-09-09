556b35d8696ddcfb19c61007ccdbfec670afccc7
Angel <santoyox714@gmail.com>
2022-09-09 13:30:06 -0700

    Update projects.ts

--- src/lib/utils/projects.ts
+++ src/lib/utils/projects.ts
@@ -2,42 +2,54 @@ export const projects = [
 	{
 		heading: 'AcmCSUF.com Website',
 		content:'Made changes to ACM Fullerton club website through github, used NodeJS and Svelte-kit to incorporate new features.',
-		link: 'https://acmcsuf.com/'
+		link: 'https://acmcsuf.com/',
+		image: '',
 	},
 	{
 		heading: 'Image Compressor ',
 		content:
 			"Browser image file size compressor made using react.",
-		link: 'https://image--compressor.vercel.app/'
+		link: 'https://image--compressor.vercel.app/',
+		image: '',
+
 	},
 	{
 		heading: 'CSS Gradient Generator ',
 		content:
 			"Webpage that displays some of my favor color gradients, allows you to generate new gradients and css code for them.",
-		link: 'https://color-craze.vercel.app/'
+		link: 'https://color-craze.vercel.app/',
+		image: '',
+
 	},
 	{
 		heading: 'Flower game Jam Video Game',
 		content:
 			"Participated in game jam hosted by CSUF’s  video game development club, and worked in a two man team over 24 hours to create a complete video game.",
-		link: 'https://itch.io/c/2469944/spring-2022-vgdc-flower-jam'
+		link: 'https://itch.io/c/2469944/spring-2022-vgdc-flower-jam',
+		image: '',
+
 	},
 	{
 		heading: 'Pixel Painter',
 		content:
 			"Paint pixels on a grid and export your painting as an image/svg",
-		link: 'https://pixel-painter.vercel.app/'
+		link: 'https://pixel-painter.vercel.app/',
+		image: '',
+
 	},
 	{
 		heading: ' Electric Vehicle Savings Website',
 		content: 'Allows user to see potential savings from switching from gasoline to electric vehicles through backend calculations and certain user inputs,',
-		link: 'https://ev-savings-frontend.vercel.app/'
+		link: 'https://ev-savings-frontend.vercel.app/',
+		image: '',
+
 	},
 	{
 		heading: 'Fun Finder ',
 		content:
 			"Get some random ideas to go out and have some fun with your date or friends!",
-		link: 'https://fun--finder.vercel.app/'
+		link: 'https://fun--finder.vercel.app/',
+		image: '',
 	},
 	
 ];
