45320d2922889ad9a036c8d1c288373f5818d741
Angel <santoyox714@gmail.com>
2022-10-08 13:42:42 -0700

    add image in about me

--- src/lib/pages/About.svelte
+++ src/lib/pages/About.svelte
@@ -5,6 +5,8 @@
   import { primaryBackground } from "$lib/utils/constants";
 
   export let backgroundClass = primaryBackground;
+
+
 </script>
 
 <Page id="about" title=" " {backgroundClass}>
@@ -12,16 +14,20 @@
     <section id = About> 
       <div class="about-container">
          <section>
-               <h2 class="about-title">About me</h2><br>
-               <div class = "header-image"></div>
+          
+               <div class="about-title">About me</div>
+
+               <div class = "header-image">   <img src = 'profile.png'width="250px"  alt=linkedin/>    </div>                  
+                      
+               
                <div class="about-subtitle">
-                   <p>
+                   
                   I'm a first generation student majoring in computer science at California State University, Fullerton. <br><br>
                   
                   I really enjoy learning new concepts on a regular basis, some fields I find pretty interesting to research in my free time are web development, game development, and occasionally cyber security. <br><br>
                  
                   Outside of school I like to play video games, learn about cars, lift weights, listen to many types of music genres, and try new foods. My favorite game franchise is the Elder Scrolls series and my favorite food is Sushi! 
-                   </p>     
+                    
              </div>
            </section>
       </div>
@@ -43,17 +49,21 @@
   .about-title {
        font-size: 3.0rem;
        font-weight: 650;
-    
+       padding-left:3rem;
+      padding-top:10px;
        color:rgb(215, 219, 223);
-       text-align: center;
    }
    
    .about-subtitle {
-       margin-top: -2rem;
        width: 70%;
        font-weight: 500;
        font-size: 1.5rem;
        line-height: 2rem;
        color:rgb(215, 219, 223);
    }
+ 
+ img{
+  border-radius: 10%;
+  margin-left:2rem;
+ }
 </style>
