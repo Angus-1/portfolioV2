ca2a9396b8690eddb81ec0ac0d87dd3283ccfbe2
Angel <santoyox714@gmail.com>
2023-10-15 02:33:56 -0700

    update projects

--- src/lib/utils/projects.ts
+++ src/lib/utils/projects.ts
@@ -6,12 +6,40 @@ export const projects = [
 		image: 'projects/blog.png',
 	},
 	{
+		heading: 'Music Journal',
+		content:'A list of music I have seen live so I can remember because I am forgetful',
+		link: 'https://live-journal.angs.dev/',
+		image: 'projects/journal.png',
+	},
+	{
 		heading: 'Image Compressor ',
 		content:
 			"Minimize file size of images in bytes without degrading image quality below an acceptable threshold",
 		link: 'https://image--compressor.vercel.app/',
 		image: 'projects/compressor.png',
 
+	},
+	{
+		heading: 'Pants runner video game',
+		content:
+			"Integrated collision mechanics, as well as procedurally generated randomization spawns of objects, in C#/Unity for CSUF Video Game Dev club Game Jam",
+		link: 'https://itch.io/queue/c/2469944/spring-2022-vgdc-flower-jam?game_id=1491257',
+		image: 'projects/game.png',
+	},
+	{
+		heading: 'Pixel Painter',
+		content:
+			"Paint pixels on a 16x16 grid and export your art as an image/svg",
+		link: 'https://pixel-painter.vercel.app/',
+		image: 'projects/painter.png',
+
+	},
+	{
+		heading: 'Date idea picker ',
+		content:
+			"Get some random ideas to go out and have some fun with your date or friends!",
+		link: 'https://fun--finder.vercel.app/',
+		image: 'projects/idea.png',
 	},
 	{
 		heading: 'Serverless File storage',
@@ -33,50 +61,6 @@ export const projects = [
 			"chatGPT chatbot that acts like a giga chad, hence the name chadGPT",
 		link: 'https://chadgpt.monkeyy.net/',
 		image: 'projects/chadGPT.png',
-	},
-	{
-		heading: 'Object Detection',
-		content:
-			"Object detection webpage that tries to localize and identify multiple objects in a single image snapshot",
-		link: 'https://object-detectionz.vercel.app/',
-		image: 'projects/detect.png',
-	},
-	{
-		heading: ' CSS Gradient Generator ',
-		content:
-			"Displays some of my favor color gradients, allows you to generate new gradients and color codes for them.",
-		link: 'https://colorz.angs.dev/',
-		image: 'projects/gradient.png',
-
-	},
-	{
-		heading: 'CSUF Game Jam',
-		content:
-			"Integrated collision mechanics, as well as procedurally generated randomization spawns of objects, in C#/Unity",
-		link: 'https://itch.io/queue/c/2469944/spring-2022-vgdc-flower-jam?game_id=1491257',
-		image: 'projects/game.png',
-	},
-	{
-		heading: 'Pixel Painter',
-		content:
-			"Paint pixels on a 16x16 grid and export your art as an image/svg",
-		link: 'https://pixel-painter.vercel.app/',
-		image: 'projects/painter.png',
-
-	},
-	{
-		heading: ' EV savings',
-		content: 'See potential savings from switching from gasoline to electric vehicles through backend calculations and certain user inputs,',
-		link: 'https://ev-savings-frontend.vercel.app/',
-		image: 'projects/evSavings.png',
-
-	},
-	{
-		heading: 'Date idea picker ',
-		content:
-			"Get some random ideas to go out and have some fun with your date or friends!",
-		link: 'https://fun--finder.vercel.app/',
-		image: 'projects/idea.png',
 	},
 	{
 		heading: 'Resources',
